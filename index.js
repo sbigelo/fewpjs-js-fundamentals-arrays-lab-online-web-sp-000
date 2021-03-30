a0ec17cd65081d945b21fdf2cb7cf33696cb16a3
sbigelo <sean.bigelow1@gmail.com>
2021-03-30 17:24:02 -0400

    commit tree 040c08108cd97fca48a0746ddb6aa1268c584571

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution here!
+const append = ["Milo", "Otis", "Garfield"]
+const prepend = ["Milo", "Otis", "Garfield"]
+const removeLast = ["Milo", "Otis", "Garfield"]
+const removeFirst = ["Milo", "Otis", "Garfield"]
+
+
+
+prepend.unshift('Odie')
+append.push('Odie')
+removeFirst.shift()
+removeLast.pop()
